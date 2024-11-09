fad4e78d57a728aa05a75364674113dd1b76c672
saketkulkarni71 <145561666+saketkulkarni71@users.noreply.github.com>
2024-11-09 22:47:28 +0000

    minimal edit to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName : 'Saket'};
+
   return (
-    <div>Dashboard</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type = 'greeting'
+            title = 'Welcome'
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your account through bonke"
+          
+          />
+        </header>
+
+      </div>
+    </section>
   )
 }
-
 export default Home
 
 
